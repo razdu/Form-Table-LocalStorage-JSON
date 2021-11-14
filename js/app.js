ef62755592413bbe3a8ceb75da20887a1cc9a523
Raz Du <razd@kahane.co.il>
2021-11-14 16:00:20 +0200

    form set

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let selectedRow = null;
+
+
+function onSubmit(e) {
+    console.log('submitted!');
+    getFormData();
+}
+
+function getFormData(){
+    let inputData = document.querySelectorAll(input)
+}
